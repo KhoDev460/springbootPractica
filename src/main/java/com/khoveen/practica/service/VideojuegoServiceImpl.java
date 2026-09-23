59415754fa4657b06010bfe06bd569429fdc4651
Kevin Figueroa <kevinignacio47015@gmail.com>
2026-09-23 19:40:50 -0300

    se implementaron los metodos incompletos

--- src/main/java/com/khoveen/practica/service/VideojuegoServiceImpl.java
+++ src/main/java/com/khoveen/practica/service/VideojuegoServiceImpl.java
@@ -1,12 +1,14 @@
 package com.khoveen.practica.service;
 
+import com.khoveen.practica.model.Videojuego;
 import com.khoveen.practica.repository.VideojuegoRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class VideojuegoServiceImpl implements VideojuegoService {
-
-    // TODO finalizar esto (incompleto)
     private final VideojuegoRepository repository;
 
     public VideojuegoServiceImpl(VideojuegoRepository repository) {
@@ -14,4 +16,28 @@ public class VideojuegoServiceImpl implements VideojuegoService {
     }
 
 
+    @Override
+    public Optional<Videojuego> obtenerPorId(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Videojuego> obtenerTodos() {
+        return List.of();
+    }
+
+    @Override
+    public Videojuego guardarJuego(Videojuego videojuego) {
+        return null;
+    }
+
+    @Override
+    public void eliminarJuegoPorId(long id) {
+
+    }
+
+    @Override
+    public List<Videojuego> buscarPorGenero(String genero) {
+        return List.of();
+    }
 }
